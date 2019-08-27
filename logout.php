1e0fffb1d433eb95f274d5f96f544bc56fb2bc28
Buwaneka Sudheera Thisarasinghe <you@example.comsudheerabuwaneka@gmail.com>
2019-08-27 22:26:15 +0530

    Customized code

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php require_once("includes/sessions.php"); ?>
-<?php include 'includes/db.php' ?>
-<?php
-$_SESSION["id"] = NULL;
-$_SESSION["username"] = NULL;
-header("Location: index.php");
-?>
